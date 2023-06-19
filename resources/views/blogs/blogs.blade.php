c4e0a7453243e4f9b5bc9ffb6afdb9773e650378
MennaRaafat <menna.raafat.hamed@commerce.helwan.edu.eg>
2023-06-20 02:09:43 +0300

    Post Blade

---
+++ resources/views/blogs/blogs.blade.php
@@ -0,0 +1,16 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+<div class="card">
+  <div class="card-header">
+    Menna Raafat
+  </div>
+  <div class="card-body">
+    <h5 class="card-title">Post title</h5>
+    <p class="card-text">Post Content</p>
+    <!-- <a href="#" class="btn btn-primary">Go somewhere</a> -->
+  </div>
+</div>
+</div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/blogs', [App\Http\Controllers\BlogController::class, 'index'])->name('blogs')->middleware('auth');
+
